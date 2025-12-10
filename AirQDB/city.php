6ce8f86075286df9e168a1cb8e112161845f0144
Mykola Plikhtiak <plikhtiak.mykola@comp-sc.if.ua>
2025-12-10 19:38:55 +0200

    Update city.php

--- AirQDB/city.php
+++ AirQDB/city.php
@@ -52,13 +52,7 @@
     // var_dump($data[0]['datetimeLocal']);
     // echo '</pre>';
  }
-    //  echo '<pre>';
-    // echo 'Filename: ' . $filename . "\n";
-    // echo 'City: ' . $city . "\n";
-    // echo 'Data:' . "\n";
-    // echo var_dump($data);
-    // echo '</pre>';
- 
+
 ?>
 <div id="chartContainer" style="width: 80%; height: 400px; margin: auto;">
     <canvas id="myChart"></canvas>
@@ -136,4 +130,5 @@
                             }
                         }
                     );
+
                 </script>
